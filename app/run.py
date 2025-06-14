b41ebebd8b98e8aee438a582fed8989d17100439
logg1n <loggiin13@gmail.com>
2025-06-14 14:22:59 +0300

    Initial commit

--- app/run.py
+++ app/run.py
@@ -1,5 +1,3 @@
-from flask import Flask
-from routes import routes  # Импортируем Blueprint
 import asyncio
 import os
 
@@ -15,17 +13,12 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher()
 
-
-app = Flask(__name__)
-app.register_blueprint(routes)  # Регистрируем маршруты
-
 async def main():
 	dp.include_router(router)
 	await dp.start_polling(bot)
 
 if __name__ == '__main__':
 	try:
-		app.run()
 		asyncio.run(main())
 	except KeyboardInterrupt:
 		print('Exit')
